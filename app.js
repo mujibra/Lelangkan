b392e7b0a09cbd83cc650f374670c50868b91a6c
Rio Lukman Tawekal <riolukmant@gmail.com>
2022-02-24 12:26:52 +0700

    + file migrate tapi belum tambah seeders, + router

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const routes = require('./routes/index')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended : false}))
+
+app.use('/', routes)
+
+app.listen(port, (err, data) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log(`Gaskeun ${port}`)
+    }
+})
---
+++ migrations/20220224045319-create-profile.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      profileId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
